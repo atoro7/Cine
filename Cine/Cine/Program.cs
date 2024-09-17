2a4a1c24d37490b7b9235e8dc8ae5a31549dbd9e
Nicolas Toro <168127995+atoro7@users.noreply.github.com>
2024-09-17 15:26:53 -0300

    Add files via upload

---
+++ Cine/Cine/Program.cs
@@ -0,0 +1,28 @@
+﻿using Cine.Enums;
+using Cine.Modelos;
+
+namespace Cine
+{
+    class Program
+    {
+        static void Main()
+        {         
+            Pelicula peli = new Pelicula("Rey Leon", "Infantil", 90, Formato._3D_Doblada);
+
+            Sala sala = new Sala(1, peli);
+            sala.DefinirHorario(16,30);
+
+            Asiento asiento = new Asiento('A', 23, TipoAsiento.Estandar, false);
+
+            Cinema cine = new Cinema("Libertad");
+
+            cine.AgregsarSala(sala);
+
+            Entrada entrada = new Entrada(cine, peli, asiento);
+            entrada.AgregarFecha(new DateTime(2024, 9, 10));
+
+            entrada.MostrarDetalles();
+            Console.WriteLine($"Horario: {sala.Horario}");
+        }
+    }
+}
